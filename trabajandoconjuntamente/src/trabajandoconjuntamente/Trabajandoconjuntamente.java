d22c492ca349f84d6ee40bd6a64669c5d9938ec3
ddiazcerezo <ddiazcerezo@eniac08.danielcastelao.org>
2014-12-15 10:06:44 +0100

    final

---
+++ trabajandoconjuntamente/src/trabajandoconjuntamente/Trabajandoconjuntamente.java
@@ -0,0 +1,19 @@
+package trabajandoconjuntamente;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author dseijasiglesias
+ */
+public class Trabajandoconjuntamente {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hola, soy mica");
+        System.out.println("Buenas, y yo Seijas");
+        System.out.println("equipo de los cojones validado");
+        JOptionPane.showMessageDialog(null, "Pull automatico");
+    }
+    
+}
