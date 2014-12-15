b72bd25440c39ca4bae816fa543f4f7325ae9824
ddiazcerezo <ddiazcerezo@eniac08.danielcastelao.org>
2014-12-15 09:10:27 +0100

    refactorizado

--- ExamenCOD/src/examencod/ExamenCOD.java
+++ ExamenCOD/src/examencod/ExamenCOD.java
@@ -15,33 +15,37 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
- int iN = 11;
-            	if (metodoMio(iN))
-System.out.println("Es numero primo");
-else
- System.out.println("NO es numero primo");           	 
-          	 
-for (int x=2;x<1000;x++) {
-if (metodoMio(x))
-System.out.print(x + " ");                         	 
-}         	 
-	}
-public static boolean metodoMio(int variable){
-return  metodoCambiado(variable);
-  } 	 
+        int iN = 11;
+        if (metodoMio(iN)) {
+            System.out.println("Es numero primo");
+        } else {
+            System.out.println("NO es numero primo");
+        }
+
+        for (int x = 2; x < 1000; x++) {
+            if (metodoMio(x)) {
+                System.out.print(x + " ");
+            }
+        }
+    }
+
+    public static boolean metodoMio(int variable) {
+        return metodoCambiado(variable);
+    }
 
     private static boolean metodoCambiado(int variable) {
         final int numero = 2;
         int j = numero;
-        boolean aux=true;
-        while ((aux) && (j!=variable)){
-            if (variable % j == 0)
+        boolean aux = true;
+        while ((aux) && (j != variable)) {
+            if (variable % j == 0) {
                 aux = false;
+            }
             j++;
         }
         return aux;
     }
-    
+
 }
 
 /* Esto es una locura
