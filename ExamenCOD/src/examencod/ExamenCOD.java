56799024654a9cf416dc056925abd6f117cee7eb
ddiazcerezo <ddiazcerezo@eniac08.danielcastelao.org>
2014-12-15 09:24:54 +0100

    final

--- ExamenCOD/src/examencod/ExamenCOD.java
+++ ExamenCOD/src/examencod/ExamenCOD.java
@@ -32,7 +32,9 @@ public class ExamenCOD {
     public static boolean metodoMio(int variable) {
         return metodoCambiado(variable);
     }
-
+    
+/*Para formatear el codigo la manera mas simple seria pulsar simultaneamente SHIFT-ALT-F,para cambiar el nombre de variable o el metodo tendriamos que clicar en refactor y posteriormente introduce,dentro de introduce nos aparecera variable 
+    y methodo lo unico que tendriamos que hacer seria clicar en cada uno de ellos y ponerles el nuebo nombre */
     private static boolean metodoCambiado(int variable) {
         final int numero = 2;
         int j = numero;
